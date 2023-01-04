11f257dedf14df60e330c6fe9db49338ca3457f1
Austin Matzko <austin@pressedcode.com>
2023-01-03 20:30:27 -0500

    Fix export for TypeScript

--- src/remove.ts
+++ src/remove.ts
@@ -1,15 +1,17 @@
-// @flow
-
 import {curry} from "flow-static-land/lib/Fun";
 
 /**
  * Returns a new Array with the result of having removed the specified amount
  * (count) of elements at the given index.
  */
-const remove = <Element>(
+const removeF = function(
   index: number,
   count: number,
   array: Array<Element>
-): Array<Element> => [...array.slice(0, index), ...array.slice(index + count)];
+): Array<Element> {
+  return [...array.slice(0, index), ...array.slice(index + count)]
+}
 
-export default curry(remove);
+const remove = curry(removeF)
+
+export { remove };
